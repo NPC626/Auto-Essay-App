5497b2a53dccee77f6de5278ad91293f7e07bdf7
NPC626 <92010861+NPC626@users.noreply.github.com>
2022-02-27 13:47:19 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pandas as pd
+
+from transformers import pipeline
+generator = pipeline('text-generation', model='EleutherAI/gpt-neo-2.7B') # Second line
+
+
+st.write("""
+# Auto-Essay App
+This app produces a essay for the given keywords!
+""")
+st.write('---')
+
+# Loads the Boston House Price Dataset
+# boston = datasets.load_boston()
+# X = pd.DataFrame(boston.data, columns=boston.feature_names)
+# Y = pd.DataFrame(boston.target, columns=["MEDV"])
+
+# Sidebar
+# Header of Specify Input Parameters
+st.sidebar.header('Specify Input Parameters')
+
+def user_input_features():
+    LEN = st.sidebar.slider('LEN', 10, 10000)
+    data = LEN
+
+    return LEN
+
+text_len = user_input_features()
+
+# Main Panel
+
+# Print specified input parameters
+st.header('Specify Input parameters')
+st.text_input("Your phrase", key="phrase_input")
+prompt = st.session_state.phrase_input
+# prompt = "The chernobyl disaster" # Third line
+res = generator(prompt, max_length= text_len, do_sample=True, temperature=0.9) # Fourth line
